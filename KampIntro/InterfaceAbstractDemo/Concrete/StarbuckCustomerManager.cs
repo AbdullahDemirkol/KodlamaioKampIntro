e7b016a76556bb6dd05f19deea0fc065a86f975b
abdullahdemirkol01@gmail.com <abdullahdemirkol01@gmail.com>
2021-03-24 19:15:51 +0300

    C#

---
+++ KampIntro/InterfaceAbstractDemo/Concrete/StarbuckCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbuckCustomerManager : BaseCustomerManager
+    {
+        private ICustomerCheckService _customerCheckService;
+        public StarbuckCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+        public override void Save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid person"); 
+            }
+        }
+    }
+}
---
+++ KampIntro/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            BaseCustomerManager customerManager1 = new StarbuckCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(2000,01,15),
+                FirstName = "Abdullah",
+                LastName = "Demirkol",
+                NationalityId = "123",
+            }); 
+            customerManager1.Save(new Customer
+            {
+                DateOfBirth = new DateTime(2000, 01, 15),
+                FirstName = "Abdullah",
+                LastName = "Demirkol",
+                NationalityId = "123",
+            });
+            Console.WriteLine();
+        }
+    }
+}
